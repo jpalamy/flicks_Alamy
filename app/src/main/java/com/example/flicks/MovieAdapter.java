b90d4644adcad73575afd3c75500fccd89300654
jpala <jpalamy@gmail.com>
2019-04-12 11:32:24 -0700

    first commit in the new machin 2.3

--- app/src/main/java/com/example/flicks/MovieAdapter.java
+++ app/src/main/java/com/example/flicks/MovieAdapter.java
@@ -1,24 +1,62 @@
 package com.example.flicks;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.flicks.models.Movie;
 
 import java.util.ArrayList;
 
-public class MovieAdapter {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
     // list of Movie
     ArrayList<Movie> movies;
 
+    // creates and inflates a new view
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    // blinds and inflated view to a new item
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    // returns the total number of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
 
     // initialize with list
     public MovieAdapter(ArrayList<Movie> movies) {
         this.movies = movies;
+
+
     }
 
-    // create the viewHolder as a static inner class
-    public static class  ViewHolder extends RecyclerView, RecyclerView.ViewHolder{
+    // create the viewholder as a static inner class
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
+        // track view objets
+        ImageView ivPosterImage;
+        TextView tVTitle;
+        TextView tvOverview;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // lookup view objets by id
+            ivPosterImage = itemView.findViewById(R.id.ivPosterImage);
+            tvOverview = itemView.findViewById(R.id.tVOverview);
+            tVTitle = itemView.findViewById(R.id.tVTitle);
+
+        }
     }
 }
