2b478c1f4fd3455f1da864267488015a0500f280
jpala <jpalamy@gmail.com>
2019-04-26 16:10:09 -0700

    first commit in the new machin 2.3.1

--- app/src/main/java/com/example/flicks/MovieAdapter.java
+++ app/src/main/java/com/example/flicks/MovieAdapter.java
@@ -1,8 +1,11 @@
 package com.example.flicks;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,7 +13,7 @@ import com.example.flicks.models.Movie;
 
 import java.util.ArrayList;
 
-public class MovieAdapter {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
     // list of Movie
     ArrayList<Movie> movies;
@@ -21,10 +24,30 @@ public class MovieAdapter {
         this.movies = movies;
     }
 
+    // creates and inflates a new view
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        // get the context and create the inflater
+        return null;
+    }
+
+    // blinds and inflated view to a new item
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    // returns the total number of items in the list
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     // create the viewHolder as a static inner class
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        // track view objets
+        // track view objects
         ImageView ivPosterImage;
         TextView tVTitle;
         TextView tvOverview;
@@ -33,6 +56,9 @@ public class MovieAdapter {
             super(itemView);
             // lookup view objets by id
             ivPosterImage = itemView.findViewById(R.id.ivPosterImage);
+            tvOverview = (TextView) itemView.findViewById(R.id.tVOverview);
+            tVTitle = (TextView) itemView.findViewById(R.id.tVTitle);
+
             
 
         }
